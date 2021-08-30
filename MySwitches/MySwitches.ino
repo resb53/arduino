d328c549439a51d722a6e28285e4ff306d103655
resb53 <resb53.git@gmail.com>
2021-08-30 20:37:34 +0000

    Annotate code

--- MySwitches/MySwitches.ino
+++ MySwitches/MySwitches.ino
@@ -1,33 +1,37 @@
 //www.elegoo.com
 //2016.12.08
 
+// # Constants #
+const int LEDSIZE = 3;
+const int maxval = pow(2, LEDSIZE);
+
+// Define Hardware
 int redPin = 5;
 int grnPin = 6;
 int bluPin = 7;
 int buttonApin = 9;
 int buttonBpin = 8;
 
+// Define System States
 unsigned int counter = 0;
-
+// - LED States: R, G, B
+int ledState[LEDSIZE] = {0, 0, 0};
+// - Button States
 int stateA = 0;
 int stateB = 0;
 int lastA = 0;
 int lastB = 0;
 
-// ledState array: R, G, B
-const int LEDSIZE = 3;
-int ledState[LEDSIZE] = {0, 0, 0};
-const int maxval = pow(2, LEDSIZE);
-int redState = 0;
-int grnState = 0;
-int bluState = 0;
+// # Functions #
 
+// Send current state to LED hardware
 void ledUpdate() {
   digitalWrite(redPin, ledState[2]);
   digitalWrite(grnPin, ledState[1]);
   digitalWrite(bluPin, ledState[0]);
 }
 
+// Set LED states to given value
 void ledSet(int value) {
   int newState[LEDSIZE] = {0, 0, 0};
   updateState(value, LEDSIZE, newState);
@@ -37,6 +41,7 @@ void ledSet(int value) {
   Serial.println();
 }
 
+// Break int into bits (Requires null array)
 void updateState(int value, int length, int state[LEDSIZE]) {
   length -= 1;
   int div = value / 2;
@@ -46,6 +51,7 @@ void updateState(int value, int length, int state[LEDSIZE]) {
   state[length] = value % 2;
 }
 
+// Catch state change for button
 int buttonPress(int button, int last, int value) {
   int buttonState = digitalRead(button);
   if (buttonState != last) {
@@ -60,6 +66,9 @@ int buttonPress(int button, int last, int value) {
   return buttonState;
 }
 
+// # Arduino #
+
+// Init
 void setup() {
   // Serial.begin(9600);
   pinMode(redPin, OUTPUT);
@@ -72,6 +81,7 @@ void setup() {
   ledUpdate();
 }
 
+// Main Loop
 void loop() { 
   lastA = buttonPress(buttonApin, lastA, 1);
   lastB = buttonPress(buttonBpin, lastB, -1);
